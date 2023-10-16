eb5ded4d8eb79c0523f51b57deb0f2c348e49751
allimak55 <147518756+allimak55@users.noreply.github.com>
2023-10-16 21:57:30 +0300

    Add files via upload
    Решение задачи №3

---
+++ HW3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+    int x, y, R, r;
+    std::cout << "Enter point coordinates \n";
+    std::cin >> x;
+    std::cin >> y;
+    std::cout << "Enter the radius of the outer circle\n";
+    std::cin >> R;
+    std::cout << "Enter the radius of the inner circle\n";
+    std::cin >> r;
+    if ((x * x + y * y) <= (R * R)) {
+        if ((x * x + y * y) >= (r * r)){
+            std::cout << "The point falls into the ring";
+        } else {
+            std::cout << "The point doesn't fit into the ring";
+        }
+    } else {
+        std::cout << "The point doesn't fit into the ring";
+    }
+
+}
