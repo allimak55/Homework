e250534e328100e0d1cb39980b2097dae20f5c16
allimak55 <147518756+allimak55@users.noreply.github.com>
2023-10-16 20:19:57 +0300

    Add files via upload
    Задача 2

---
+++ HW1.cpp
@@ -0,0 +1,20 @@
+#include  <iostream>
+#include <cmath>
+
+int main(){
+    double a, b, c;
+    double x1, x2, D;
+    std::cout << "Please enter the coefficients of the equation\n";
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> c;
+    D = b * b - 4 * a * c;
+    if (D < 0){
+        std::cout << "Sorry, no roots\n";
+    } else if (D == 0){
+        std::cout << "One root = " << -b / (2 * a);
+    } else {
+        std::cout << "One root = " << (-b + sqrt(D)) / (2 * a) << "\n";
+        std::cout << "Two root = " << (-b - sqrt(D)) / (2 * a) << "\n";
+    }
+}
